d940f3ec844bcfd6c5e0a00e34ac4fdc8ed3b0ec
Martin Hoehne <martin-hoehne@live.de>
2022-01-16 17:36:13 +0100

    added mongoose connection

--- index.ts
+++ index.ts
@@ -11,6 +11,10 @@ const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.get('/accounts', accountController);
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo:27017/todo', {useNewUrlParser: true}).catch(function(){
+  process.exit(1)
+})
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
