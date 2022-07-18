6d9f561f72a753a793a7481c199babaf2e06c792
Martin Hoehne <martin-hoehne@live.de>
2022-07-18 19:44:30 +0200

    added state query

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -187,7 +187,8 @@ export async function listProduct(req: Request, res: Response) {
   }
 
   // START ### FILTER ON SEARCH PAGE ###
-  const query: { [key: string]: boolean | number } = {};
+  const query: { [key: string]: boolean | number | string } = {};
+  const state = (req.query.state as string) ?? '';
   const account_id = (req.query.account_id as string) ?? '';
   const zielgruppe = (req.query.zielgruppe as string[]) ?? [];
   const anwendungsbereich = (req.query.anwendungsbereich as string[]) ?? [];
@@ -196,8 +197,11 @@ export async function listProduct(req: Request, res: Response) {
   const systemgrenzen = (req.query.systemgrenzen as string[]) ?? [];
   const betrachtungskonzept = (req.query.betrachtungskonzept as string[]) ?? [];
 
+  if (state.length !== 0) {
+    query['state'] = state;
+  }
+
   if (account_id.length !== 0) {
-    console.log(account_id);
     query['account_id'] = parseInt(account_id);
   }
 
